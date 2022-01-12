e218429fdf9f5986c94be89dc1d721eac179d726
Alwin Hauser <alwin.hauser@edu.tbz.ch>
2022-01-12 16:42:34 +0100

    GPS FINE_LOCATION Permission added Imagebuttons - switching between Activities now works Speedometer in M/s works

--- app/src/main/java/ch/hauseralwin/blitzeroni/MainActivity.java
+++ app/src/main/java/ch/hauseralwin/blitzeroni/MainActivity.java
@@ -1,14 +1,65 @@
 package ch.hauseralwin.blitzeroni;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    private ImageButton openSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        openSettings = (ImageButton) findViewById(R.id.imageButton_openSettings);
+        openSettings.setOnClickListener(v -> openSettingsActivity());
+
+
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED &&ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED)
+    {
+        // TODO: Consider calling
+        //    ActivityCompat#requestPermissions
+        // here to request the missing permissions, and then overriding
+        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+        //                                          int[] grantResults)
+        // to handle the case where the user grants the permission. See the documentation
+        // for ActivityCompat#requestPermissions for more details.
+        return;
+    }
+        LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,this);
+
+}
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+
+        TextView txt = (TextView) this.findViewById(R.id.textView_CurrentSpeed);
+
+        if (location==null) {
+            txt.setText("### KMH");
+        } else {
+            float nCurrentSpeed = location.getSpeed();
+            txt.setText(nCurrentSpeed + " KMH");
+        }
+
+    }
+    public void openSettingsActivity() {
+        Intent intent = new Intent(this, SettingActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/ch/hauseralwin/blitzeroni/SettingActivity.java
+++ app/src/main/java/ch/hauseralwin/blitzeroni/SettingActivity.java
@@ -2,13 +2,25 @@ package ch.hauseralwin.blitzeroni;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class SettingActivity extends AppCompatActivity {
+    private ImageButton backToMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        backToMain = (ImageButton) findViewById(R.id.imageButton_backToMain);
+        backToMain.setOnClickListener(v -> openMainActivity());
+
+    }
+
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
